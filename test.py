ec63a0c73f508f6079b2533adb256f6dbe4b3bd2
erguzel <erguzel@itu.edu.tr>
2019-12-08 13:30:55 +0100

    review sample

--- test.py
+++ test.py
@@ -1,10 +1,25 @@
 import os,re,codecs
+import numpy
+
+a = numpy.array([[1, 2, 3]])
+
+asd = a[0]
+
+zerro = numpy.zeros(10)
+
+newasd = numpy.concatenate([asd,zerro])
+
+a = numpy.array([newasd])
+
+print(newasd)
+
 
 withHeader = False
 inputPath = "data/raw/bbc 4"
 targetPath = "data/processed"
 inputExtension= 'txt'
 outputExtension='csv'
+outputFolderName='bbc4'
 label=""
 index=0
 separator=">>"
@@ -15,6 +30,7 @@ dataPreparer.create_multiclassifier_data(inputPath,
                                          targetPath,
                                          inputExtension,
                                          outputExtension,
+                                         outputFolderName,
                                          separator,
                                          withHeader)
 
