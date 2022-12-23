b38fae1707eb64cee808c54486274faab44e1012
erguzel <erguzel@itu.edu.tr>
2022-12-23 17:59:22 +0100

    add model pkg

---
+++ src/aistudio/model/model_utils.py
@@ -0,0 +1,29 @@
+from exchelp.exception_helper import ReportObject,CoreException
+from joblib import dump 
+from pathlib import Path
+from os import path,getcwd
+from datetime import datetime
+import os
+
+
+def persist_ml_model(modelName:str,trainedModel,runTitle:str,resultDir:str=None,mainReport:ReportObject=None):
+    """persists a trained ML model to the disk
+
+    Args:
+        modelName (str): name of the model
+        trainedModel (_type_): trained model object
+        main_report (ReportObject): Report object associated with the model train process
+        file (str): the main file of execution to use as the name of the output folder
+    """
+    try:
+        current_datetime  = datetime.now().strftime("_%d-%b-%Y_%H_%M_%S")
+        model_dir = path.join(getcwd() if resultDir== None else resultDir,'models',os.path.basename(runTitle),current_datetime)
+        Path(model_dir).mkdir(parents=True, exist_ok=True)
+        model_full_name = path.join(model_dir,modelName)
+        dump(trainedModel,model_full_name)
+        report_full_name = path.join(model_dir,'report.json')
+        if(mainReport!=None):
+            with open(report_full_name, 'w') as f:
+                f.write(mainReport.reportize())
+    except Exception as e:
+        CoreException('persist_model failed',e,logIt=True,dontThrow=True,shouldExit=True).adddata('locals',str(locals())).act()
