93fdf60aa9d68ddbea684d2aaeb21ed6618a3900
erguzel <erguzel@itu.edu.tr>
2023-06-12 18:53:08 +0200

    add stats util module

---
+++ src/aistudio/statistics/stats_utils.py
@@ -0,0 +1,30 @@
+import scipy.stats as stats
+import numpy as np
+
+def get_outlier_range(data):
+    """
+    Get the range of outliers in a dataset.
+    
+    Parameters
+    ----------
+    data : array-like
+        The dataset.
+    
+    Returns
+    -------
+    outlier_range : tuple
+        The range of outliers in the dataset.
+    """
+    
+    # Get the quartiles
+    q1, q3 = np.percentile(data, [25, 75])
+    iqr = q3-q1
+    
+    # Get the lower and upper bounds
+    lower_bound = q1 - (1.5 * iqr)
+    upper_bound = q3 + (1.5 * iqr)
+    
+    ranges = stats.percentileofscore(data, [lower_bound, upper_bound])
+
+    return ranges
+    
