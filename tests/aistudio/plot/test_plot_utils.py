9fafb93e3fb13a422bfbc1e7177ddef939c1f55f
erguzel <erguzel@itu.edu.tr>
2023-06-22 23:50:20 +0200

    refactor all with base types, before changing branch name to main

--- tests/aistudio/plot/test_plot_utils.py
+++ tests/aistudio/plot/test_plot_utils.py
@@ -18,8 +18,16 @@ df_original = pd.read_pickle('examples/dscienc-cstudy/Uber-Pickups-Weather/bin.u
 df = df_original.copy()
 
 
-fig = pu.multi_boxplot(
-    plotparam=pu.PlotParam(data = df, x = 'pickups'),
-    features=argsbase('temp','dew_point_temperature','snow_depth_inc'),
-    updateparams=argsbase(pu.FacetParam(col='is_holiday')).setmasterparam(True)
+bx = pu.multi_boxplot(
+    pu.PlotParam(data = df, x = 'pickups'),
+    features=argsbase('temp','dew_point_temperature','sea_level_pressure'),
+    dotvars= pu.kwargsbase(alpha = 0.08,pointsize = 4).setmasterparam(True),
+    jittervars= pu.argsbase(pu.kwargsbase(width = 1)),
+    meanvars = pu.argsbase(pu.kwargsbase(color = 'cyan',linestyle = '--')),
+    dotview = argsbase(True,True),
+    showhistogram=True,
+    histvars=pu.argsbase(None,pu.TransformParam(so.Bars(color='k'),so.Hist('density'))),
+    kdevars=pu.TransformParam(so.Area(),so.KDE(cut=0,bw_adjust=2)).setmasterparam(True),
+    showkde=True,
+    updateparams = argsbase(pu.FacetParam(col = 'is_holiday')).setmasterparam(True)
 )
