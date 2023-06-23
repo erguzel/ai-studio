5cadf8d3842e58db3c7f8d7f5d19b6e80c1e8707
erguzel <erguzel@itu.edu.tr>
2023-06-24 01:13:17 +0200

    refactor as percentile bounds

--- src/aistudio/statistics/stats_utils.py
+++ src/aistudio/statistics/stats_utils.py
@@ -1,30 +1,28 @@
 import scipy.stats as stats
 import numpy as np
 
-def get_outlier_range(data,percentile = [25,75]):
+def get_outlier_percentileofscore(data, percentile = [25.0,75.0]):
     """
-    Get the range of outliers in a dataset.
-    
-    Parameters
-    ----------
-    data : array-like
-        The dataset.
-    
-    Returns
-    -------
-    outlier_range : tuple
-        The range of outliers in the dataset.
+    Returns the percentiles of scores of lower and upper outlier boundry data points as 1.5 * IQR according to given percentiles.
     """
-    
-    # Get the quartiles
-    q1, q3 = np.percentile(data, percentile)
-    iqr = q3-q1
-    
-    # Get the lower and upper bounds
-    lower_bound = q1 - (1.5 * iqr)
-    upper_bound = q3 + (1.5 * iqr)
-    
+    lower_bound,upper_bound = get_outlier_bounds(data,percentile)
     ranges = stats.percentileofscore(data, [lower_bound, upper_bound])
 
     return ranges
     
+def get_outlier_bounds(data, percentile = [25.0,75.0]):
+    """
+    Returns the lower and upper points of (1.5 * IQR) defined by given percentiles
+    """
+    q1, q3 = np.percentile(data, percentile)
+    iqr = q3-q1
+    
+    # Get the lower and upper bounds
+    lower_bound = q1 - (1.5 * iqr) 
+    upper_bound = q3 + (1.5 * iqr)
+
+    lower_bound = lower_bound if lower_bound >= min(data) else min(data)
+    upper_bound = upper_bound if upper_bound <= max(data) else max(data)
+
+
+    return lower_bound,upper_bound
