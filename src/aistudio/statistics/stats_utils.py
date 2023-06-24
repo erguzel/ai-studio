d3a09d79e198a745eb02b0c6a5149ba38c720e21
erguzel <erguzel@itu.edu.tr>
2023-06-24 10:47:40 +0200

    refactor to argsbase type

--- src/aistudio/statistics/stats_utils.py
+++ src/aistudio/statistics/stats_utils.py
@@ -1,4 +1,8 @@
 import scipy.stats as stats
+from aistudio.abstraction.base_types import *
+from exchelp.exception_helper import *
+from inspect import currentframe, getframeinfo
+
 import numpy as np
 
 def get_outlier_percentileofscore(data, percentile = [25.0,75.0]):
@@ -25,4 +29,48 @@ def get_outlier_bounds(data, percentile = [25.0,75.0]):
     upper_bound = upper_bound if upper_bound <= max(data) else max(data)
 
 
+    
     return lower_bound,upper_bound
+
+
+def get_outlier_percentileofscores(data, percentiles:argsbase = argsbase([25.0,75.0]))->argsbase:
+    result = []
+    for i,perc in enumerate(percentiles.args):
+        if len(perc) !=2:
+            (
+               CoreException(
+                message='Only uppest and lowest percentile values are needed i.e. [25.0,75.0]',
+                logIt=True)
+                .adddata('filename',getframeinfo(currentframe()).filename)
+                .adddata('lineno',getframeinfo(currentframe()).lineno)
+                .act() 
+            )
+        lower,upper = get_outlier_boundset(data,argsbase(perc)).args[i]
+        lowerdata,upperdata = stats.percentileofscore(data,[lower,upper])
+        result.append([lowerdata,upperdata])
+    
+    return argsbase(*result)
+        
+            
+
+def get_outlier_boundset(data,percentiles:argsbase=argsbase([25.0,75.0]))->argsbase:
+    result = []
+    for perc in percentiles.args:
+        if len(perc) != 2:
+            (
+            CoreException(
+                message='Only uppest and lowest percentile values are needed i.e. [25.0,75.0]',
+                logIt=True)
+                .adddata('filename',getframeinfo(currentframe()).filename)
+                .adddata('lineno',getframeinfo(currentframe()).lineno)
+                .act()
+            )
+        q1,q3 = np.percentile(data,perc)
+        iqr = q3-q1
+        lower_bound = q1 - (1.5 * iqr)
+        upper_bound = q3 + (1.5 * iqr)
+        lower_bound = lower_bound if lower_bound >= min(data) else min(data)
+        upper_bound = upper_bound if upper_bound <= max(data) else max(data)
+        result.append((lower_bound,upper_bound))
+    return argsbase(*result)
+
---
+++ tests/aistudio/statistics/test_stat_utils.py
@@ -0,0 +1,27 @@
+import os
+import sys
+print(os.getcwd())
+sys.path.insert(1,os.getcwd())
+
+
+import pytest 
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import seaborn.objects as so
+import aistudio.plot.plot_utils as pu
+from aistudio.abstraction.base_types import *
+import aistudio.statistics.stats_utils as su
+
+df_original = pd.read_pickle('examples/dscienc-cstudy/Uber-Pickups-Weather/bin.upw-clear.df.pkl')
+df = df_original.copy()
+
+d,u = su.get_outlier_boundset(df['pickups'],argsbase([25,75])).args[0]
+print(d,u)
+
+l,u = su.get_outlier_percentileofscores(df['pickups'],argsbase([25,75])).args[0]
+print(l,u)
+
+
