092924bf6c172f52d3ed1916cf8fc0ee8a89f0ad
erguzel <erguzel@itu.edu.tr>
2023-06-24 13:17:37 +0200

    del old statfuncts

--- src/aistudio/statistics/stats_utils.py
+++ src/aistudio/statistics/stats_utils.py
@@ -5,34 +5,6 @@ from inspect import currentframe, getframeinfo
 
 import numpy as np
 
-def get_outlier_percentileofscore(data, percentile = [25.0,75.0]):
-    """
-    Returns the percentiles of scores of lower and upper outlier boundry data points as 1.5 * IQR according to given percentiles.
-    """
-    lower_bound,upper_bound = get_outlier_bounds(data,percentile)
-    ranges = stats.percentileofscore(data, [lower_bound, upper_bound])
-
-    return ranges
-    
-def get_outlier_bounds(data, percentile = [25.0,75.0]):
-    """
-    Returns the lower and upper points of (1.5 * IQR) defined by given percentiles
-    """
-    q1, q3 = np.percentile(data, percentile)
-    iqr = q3-q1
-    
-    # Get the lower and upper bounds
-    lower_bound = q1 - (1.5 * iqr) 
-    upper_bound = q3 + (1.5 * iqr)
-
-    lower_bound = lower_bound if lower_bound >= min(data) else min(data)
-    upper_bound = upper_bound if upper_bound <= max(data) else max(data)
-
-
-    
-    return lower_bound,upper_bound
-
-
 def get_outlier_percentileofscores(data, percentiles:argsbase = argsbase([25.0,75.0]))->argsbase:
     result = []
     for i,perc in enumerate(percentiles.args):
@@ -51,8 +23,6 @@ def get_outlier_percentileofscores(data, percentiles:argsbase = argsbase([25.0,7
     
     return argsbase(*result)
         
-            
-
 def get_outlier_boundpairs(data,percentiles:argsbase=argsbase([25.0,75.0]))->argsbase:
     result = []
     for perc in percentiles.args:
