9eaad5cf3f6e7ec922ad46dcf313a74d44110a8d
Olgun Erguezel <olgunerguzel@gmail.com>
2023-06-26 16:53:40 +0200

    merge with exchelp

--- tests/aistudio/exception/test_exception_utils.py
+++ tests/aistudio/exception/test_exception_utils.py
@@ -11,16 +11,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import seaborn.objects as so
+import aistudio.plot.plot_utils as pu
 from aistudio.abstraction.base_types import *
-import aistudio.statistics.stats_utils as su
+from aistudio.exception.exception_utils import *
+
+
 
 df_original = pd.read_pickle('examples/dscienc-cstudy/Uber-Pickups-Weather/bin.upw-clear.df.pkl')
 df = df_original.copy()
 
-d,u = su.get_outlier_boundpairs(df['pickups'],argsbase([25,75,100])).args[0]
-print(d,u)
 
-l,u = su.get_outlier_percentileofscores(df['pickups'],argsbase([25,75,100])).args[0]
-print(l,u)
+int = Interrupter('some error occured','dont do stg',logit = True,throw = False)
 
+int = int.addData(logit=True,bar = 'foo')
 
+print(int.kwargs)
