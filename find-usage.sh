053890bf8a304a0e68666ff99a58ffbcea390808
erguzel <olgunerguzel@gmail.com>
2026-09-12 21:58:29 +0200

    chore: drop unusable replace-text.sh and harden find-usage.sh

--- find-usage.sh
+++ find-usage.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+# List files containing the given text, skipping the git directory.
 
-find . -type f -print0 | xargs -0 grep -l $1
+find . -path ./.git -prune -o -type f -print0 | xargs -0 grep -lI "$1"
