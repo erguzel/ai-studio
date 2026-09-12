dc81e02f45696c7ffe6008451d4e1e7488fc355a
erguzel <olgunerguzel@gmail.com>
2026-09-12 22:09:25 +0200

    refactor: drop dead folder-to-csv path and its committed output

--- src/aistudio/data/text_utils.py
+++ src/aistudio/data/text_utils.py
@@ -2,68 +2,9 @@ import re as re
 import numpy as np
 from nltk.stem import WordNetLemmatizer, StemmerI
 from exchelp.exception_helper import *
-import os
 
 
 
-def tabularize_folder_labelled_text(
-    inputPath:str,
-    targetPath:str,
-    inputExtension:str,
-    outputExtension:str,
-    outputFolderName:str,
-    separator:str,
-    withHeader:bool
-)->None:
-    """Creates a tabular version of a text data labelled by different sub-folders.
-
-    Args:
-        inputPath (str): input data dir in which the text files are resides in sub-folders of different categories
-        targetPath (str): the path to save tabular data
-        inputExtension (str): text file extension, usually .txt
-        outputExtension (str): output tabular data extension, usually .csv
-        outputFolderName (str): final output folder or file name
-        separator (str): line separators
-        withHeader (bool): True to have a header
-    """
-    label = ""
-    index = 0
-    csvLine = ""
-    processedDataList = ""
-    fileName = ""
-    addedHeader = False
-
-    lastIndex = len(re.split(r"[/\\]", inputPath)) - 1
-    fileName = re.split(r"[/\\]", inputPath)[lastIndex]
-
-    files = []
-    # r=root, d=directories, f = files
-    for r, d, f in os.walk(inputPath):
-        for file in f:
-            if "." + inputExtension in file:
-                absfile = os.path.join(r, file)
-                indexLast = len(re.split(r"[/\\]", absfile)) - 1
-                label = re.split(r"[/\\]", absfile)[indexLast - 1]
-                content = open(absfile, "rb").read()
-                csvLine = "{0}{1}{2}{1}{3}".format(str(index), separator, content, label)
-                if (withHeader):
-                    if (not addedHeader):
-                        processedDataList += r"id{0}content{0}label\n".format(separator)
-                        addedHeader = True
-
-                if (not csvLine in processedDataList):
-                    processedDataList += csvLine + ';;'
-                    index = index + 1
-
-    if len(outputFolderName)> 0:
-        fileName = outputFolderName;
-    outPath = os.path.join(targetPath, fileName)
-    saveFile = "{0}.{1}".format(outPath, outputExtension)
-    open(saveFile, "w").write(processedDataList)
-    print("{0} data created with separator '{1}'.".format(os.path.abspath(saveFile),separator))
-    #
-    #
-    #
 def stemming_lematization(stem_or_lemmatizer:WordNetLemmatizer|StemmerI,data : np.ndarray)->np.ndarray:
     """Stemmizes or lemmatizes the given data according to provided stemmer or lemmatizer.
     StemmerI or WordNetLemmatizer objects are accepted currently from nltk package
