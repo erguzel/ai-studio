ecb2026ff6147d5bac68d4283be65fef16aa29da
Olgun Erguezel <olgunerguzel@gmail.com>
2026-09-13 11:23:13 +0200

    test: make the test suite runnable without nltk and plotting deps

--- tests/aistudio/data/meta/test_module_utils.py
+++ tests/aistudio/data/meta/test_module_utils.py
@@ -1,49 +1,44 @@
-import os
-import sys
-print(os.getcwd())
-sys.path.insert(1,os.getcwd())
+import json
 
-from src.aistudio.data.meta.module_utils import ModuleMeta
+import numpy as np
+import pytest
 
-import pytest 
-
-from src.aistudio.data.text_utils import *
-
-def test_module_meta():
-    data = np.array([
-        'some data without any sign',
-        'special character data',
-        'multiple spaces data',
-        'some capital letters and spaces with p3c ch ct3rs data'
-    ])
-    
-    # Case1
-    
-    actual = np.array([
-        'some data without any sign',
-        'special character data',
-        'multiple space data',
-        'some capital letter and space with p3c ch ct3rs data'
-    ])
-
-    #import aistudio.data.text_utils as tt
-    #tt.stemming_lematization(WordNetLemmatizer())
-    #ModuleMeta('nltk.stem','WordNetLemmatizer').get()
-    lemmatizer_instance = ModuleMeta(
-        moduleName='nltk.stem',
-        objectName='WordNetLemmatizer').caller()
-    res = ModuleMeta(
-        'aistudio.data.text_utils',
-        'stemming_lematization').caller
-    
-    result = res(stem_or_lemmatizer=lemmatizer_instance,data=data)
-
-    print(result)
-
-    
+from aistudio.data.meta.module_utils import ModuleMeta, object_from_module
 
 
+def test_object_from_module_resolves_a_function():
+    assert object_from_module('json', 'dumps') is json.dumps
 
-     
 
-test_module_meta()
+def test_object_from_module_resolves_a_sub_object():
+    assert object_from_module('json', 'JSONEncoder', 'default') is json.JSONEncoder.default
+
+
+def test_object_from_module_raises_on_unknown_module():
+    with pytest.raises(ImportError):
+        object_from_module('aistudio.no.such.module', 'anything')
+
+
+def test_object_from_module_raises_on_unknown_object():
+    with pytest.raises(AttributeError):
+        object_from_module('json', 'no_such_object')
+
+
+def test_module_meta_keeps_the_names_it_resolved():
+    meta = ModuleMeta(moduleName='json', objectName='dumps')
+
+    assert meta.modulename == 'json'
+    assert meta.objectName == 'dumps'
+    assert meta.subobjectName is None
+
+
+def test_module_meta_caller_runs_the_resolved_function():
+    """The declare-by-name path the examples rely on, without importing the callee."""
+    caller = ModuleMeta(
+        moduleName='aistudio.data.text_utils',
+        objectName='clear_default_chars',
+    ).caller
+
+    result = caller(data=np.array([b'multiple    spaces data']))
+
+    assert list(result) == ['multiple spaces data']
--- tests/aistudio/data/test_text_utils.py
+++ tests/aistudio/data/test_text_utils.py
@@ -1,67 +1,77 @@
-import os
-import sys
-print(os.getcwd())
-sys.path.insert(1,os.getcwd())
+import numpy as np
+import pytest
+
+from aistudio.data.text_utils import clear_default_chars, stemming_lematization
 
 
-import pytest 
+class FakeLemmatizer:
+    """Stands in for nltk's WordNetLemmatizer: strips a trailing 's'."""
 
-from src.aistudio.data.text_utils import *
+    def lemmatize(self, token):
+        return token[:-1] if token.endswith('s') else token
 
-def test_clear_default_chars():
+
+class FakeStemmer:
+    """Stands in for an nltk StemmerI: keeps the first four characters."""
+
+    def stem(self, token):
+        return token[:4]
+
+
+class FakeBoth(FakeLemmatizer, FakeStemmer):
+    """Exposes both methods, so lemmatize has to win."""
+
+
+def test_clear_default_chars_cleans_byte_texts():
     data = np.array([
         b'some data without any sign',
         b'special character data & ^',
         b'multiple    spaces data',
-        b'SoME CApital LETTERS   and  spaces WitH  $p3c!@l Ch@r@ct3rs data'
+        b'SoME CApital LETTERS   and  spaces WitH  $p3c!@l Ch@r@ct3rs data',
     ])
-    
-    #Case1
 
-    actual = np.array([
+    assert list(clear_default_chars(data=data)) == [
         'some data without any sign',
         'special character data',
         'multiple spaces data',
-        'some capital letters and spaces with p3c ch ct3rs data'
-    ])
-    expected = clear_default_chars(data=data)
-    assert len(actual) == len(expected)
-    assert all([a==b for a,b in zip(actual,expected)])
-    #
-    #
-    #
-from nltk.stem import WordNetLemmatizer, LancasterStemmer
-def test_stemming_lematization():
-    data = [
-        'some data without any sign',
-        'special character data',
-        'multiple spaces data',
-        'some capital letters and spaces with p3c ch ct3rs data'
+        'some capital letters and spaces with p3c ch ct3rs data',
     ]
-    
-    # Case1
-    
-    actual = np.array([
-        'some data without any sign',
-        'special character data',
-        'multiple space data',
-        'some capital letter and space with p3c ch ct3rs data'
-    ])
-    expected = stemming_lematization(stem_or_lemmatizer=WordNetLemmatizer(),data=data)
-    assert len(actual) == len(expected)
-    assert all([a==b for a,b in zip(actual,expected)])
-    #
-    # Case2
-    #
-    actual = np.array([
-        'som dat without any sign',
-        'spec charact dat',
-        'multipl spac dat',
-        'som capit let and spac with p3c ch ct3rs dat'
-    ])
-    expected = stemming_lematization(stem_or_lemmatizer= LancasterStemmer(),data=data)
-    assert len(actual) == len(expected)
-    assert all([a==b for a,b in zip(actual,expected)])
-    
 
-test_stemming_lematization()
+
+def test_clear_default_chars_rejects_non_arrays():
+    with pytest.raises(TypeError):
+        clear_default_chars(data=['some data'])
+
+
+def test_stemming_lematization_lemmatizes():
+    data = np.array(['multiple spaces data', 'special characters'])
+
+    result = stemming_lematization(stem_or_lemmatizer=FakeLemmatizer(), data=data)
+
+    assert list(result) == ['multiple space data', 'special character']
+
+
+def test_stemming_lematization_stems():
+    data = np.array(['multiple spaces data', 'special characters'])
+
+    result = stemming_lematization(stem_or_lemmatizer=FakeStemmer(), data=data)
+
+    assert list(result) == ['mult spac data', 'spec char']
+
+
+def test_stemming_lematization_prefers_lemmatize_over_stem():
+    data = np.array(['multiple spaces'])
+
+    result = stemming_lematization(stem_or_lemmatizer=FakeBoth(), data=data)
+
+    assert list(result) == ['multiple space']
+
+
+def test_stemming_lematization_rejects_non_arrays():
+    with pytest.raises(TypeError):
+        stemming_lematization(stem_or_lemmatizer=FakeLemmatizer(), data=['some data'])
+
+
+def test_stemming_lematization_rejects_objects_without_either_method():
+    with pytest.raises(TypeError):
+        stemming_lematization(stem_or_lemmatizer=object(), data=np.array(['data']))
